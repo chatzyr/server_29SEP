5931fb4c98264efdc5f9ea1c8d75c4ca71d316bc
chatzyr <146387929+chatzyr@users.noreply.github.com>
2023-10-09 23:58:47 +0500

    Add files via upload

---
+++ models/personalmessage.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const messageSchema = new mongoose.Schema({
+  senderId: {
+    type: String,
+    ref: 'User',
+  },
+  recepientId: {
+    type: String,
+    ref: 'User',
+  },
+  messageType: {
+    type: String,
+  },
+  message: String,
+  timeStamp: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const PersonalMessage = mongoose.model('PersonalMessage',messageSchema);
+
+module.exports = PersonalMessage;
