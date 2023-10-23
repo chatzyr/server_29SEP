efeee44ddfd709042967c8ea00444df3c0d97348
chatzyr <146387929+chatzyr@users.noreply.github.com>
2023-10-23 19:51:31 +0500

    Update user.js

--- models/user.js
+++ models/user.js
@@ -14,13 +14,15 @@ const userSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     badge: { type: String, default: 'https://cdn-icons-png.flaticon.com/512/2955/2955010.png' },
-    pic: { type: String, default: '   https://cdn-icons-png.flaticon.com/512/3177/3177440.png ' },
+    pic: { type: String, default: 'https://cdn-icons-png.flaticon.com/512/3177/3177440.png' },
     backgroundPic: { type: String, default: 'https://as2.ftcdn.net/v2/jpg/01/68/74/87/1000_F_168748763_Mdv7zO7dxuECMzItERhPzWhVJSaORTKd.jpg' },
     bio: { type: String, default: 'Hey There! I am chatZyr user!' },
     likes: { type: Number, default: 0 },
     likedBy: [{ type: String, ref: 'User'}], // Array to store user IDs who have liked this profile
-    premium: {type: Boolean, default:false},
+    premium: {type: String, default:"false"},
     friends: [friendSchema],
+    chatcolor: { type: String, default: '#FFFFFF' } // Added chatcolor field
+
 });
 
 const User = mongoose.model("User", userSchema);
