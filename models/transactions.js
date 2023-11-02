dff44124ddd868c01d49b9f970a79aea2b841ce0
chatzyr <146387929+chatzyr@users.noreply.github.com>
2023-11-02 22:31:23 +0500

    transactin model

---
+++ models/transactions.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const badgeSchema1 = new mongoose.Schema({
+    transaction_id: { type: String, required: true,unique: true },
+    amount: { type: String, required: true },
+    time: { type: String, required: true },
+    status:{type:String, default:'pending'}
+    
+});
+
+const trans = mongoose.model('transactions', badgeSchema1);
+
+module.exports = trans;
