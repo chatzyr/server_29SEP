913f61616ddb0e8fd98818b14323755551846ab8
chatzyr <146387929+chatzyr@users.noreply.github.com>
2023-11-03 21:19:21 +0500

    Update transactions.js

--- models/transactions.js
+++ models/transactions.js
@@ -5,7 +5,7 @@ const badgeSchema1 = new mongoose.Schema({
     transaction_id: { type: String, required: true,unique: true },
     amount: { type: String, required: true },
     time: { type: String, required: true },
-    status: {type:String, default:'pending'}
+    status: {type:String, default:'verified'}
     
 });
 
