f7e141dcc42a2bb97c65aabed072440e0996845e
chatzyr <146387929+chatzyr@users.noreply.github.com>
2023-11-03 16:41:23 +0500

    ssa

--- models/transactions.js
+++ models/transactions.js
@@ -1,13 +1,14 @@
 const mongoose = require('mongoose');
 
 const badgeSchema1 = new mongoose.Schema({
+    email: {type: String, default:''},
     transaction_id: { type: String, required: true,unique: true },
     amount: { type: String, required: true },
     time: { type: String, required: true },
-    status:{type:String, default:'pending'}
+    status: {type:String, default:'pending'}
     
 });
 
 const trans = mongoose.model('transactions', badgeSchema1);
 
-module.exports = trans;
+module.exports = trans;
