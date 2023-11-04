1de847b8eaf4a1776d7ee0efa168204c0d9e13c7
chatzyr <146387929+chatzyr@users.noreply.github.com>
2023-11-05 03:32:20 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1302,7 +1302,7 @@ app.get("/find-transactions/:email", async (req, res) => {
 });
 app.get("/fetchver", async (e, o) => {
   try {
-    t = { version: "1.1.5", link: "https://shorturl.at/pBDEP" };
+    t = { version: "1.1.0", link: "https://shorturl.at/pBDEP" };
     o.json(t);
   } catch (e) {
     console.error("Error:", e),
