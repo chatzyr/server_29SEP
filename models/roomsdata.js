5a384d6616dd30940185c6f96fc045d17e256cef
chatzyr <146387929+chatzyr@users.noreply.github.com>
2024-06-20 15:25:23 +0500

    Update roomsdata.js

--- models/roomsdata.js
+++ models/roomsdata.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    email: String,
+    email: {type: String, unique:true},
     x: { type: Number, default: 215 },
     y: { type: Number, default: 125 },
   });
 
 
-
+  userSchema.index({ email: 1 }, { unique: true });
 const roomSchema = new mongoose.Schema({
     roomId: { type: String, required: true, unique: true },
     users: { type: [String], default: [''] },
@@ -18,7 +18,12 @@ const roomSchema = new mongoose.Schema({
     blocked: { type: [String], default: [''] },
     badgeurl: { type: String, default: 'https://icons.veryicon.com/png/o/media/home-furnishing-icon/room-1.png' },
     videourl: { type: String, default: '' },
-    bio: { type: String, default: '' }
+    bio: { type: String, default: '' },
+    likes: {type: Number, default:0},
+    likedBy: [{ type: String, default:''}],
+    roomOwner:{type: String, default: ''},
+    public:{type: Boolean, default: true},
+    password:{type:String,default:'123'}
 });
 
 
